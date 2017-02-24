c313f64b2a1195c6a0580a5c5c17b1618ec0b0d3
Ann Wolff <annwolffe@gmail.com>
2017-02-24 10:41:51 -0700

    Update current_user method

--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -11,7 +11,8 @@ module TestHelpers
           email: "profsnape@hogwarts.edu",
           locale: "wizard world",
           gender: "male",
-          token: "ya29.GlvtA-mnI6g3bwmjaGpZaYyKo5GhMkm8yNaGjL1NmjO5LgPv1dprnLdxMIFnBZTFqyMivfbLoyz3ipcdShXK5n68GqIIL5feWayPJFs11zcQrWCgaJDgVMqfPdu-"
+          token: "ya29.GlvtA-mnI6g3bwmjaGpZaYyKo5GhMkm8yNaGjL1NmjO5LgPv1dprnLdxMIFnBZTFqyMivfbLoyz3ipcdShXK5n68GqIIL5feWayPJFs11zcQrWCgaJDgVMqfPdu-",
+          image: "https://s-media-cache-ak0.pinimg.com/originals/b7/38/f1/b738f16e69459d18b8086cf700a85bb4.jpg"
         }
       }
     )
