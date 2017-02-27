b1a7432fbb6e19bc40ce178ac1e37863a601de4d
Ann Wolff <annwolffe@gmail.com>
2017-02-27 14:55:04 -0700

    Limit abilities for productions to managers

--- spec/features/company_mangers/manager_creates_team_spec.rb
+++ spec/features/company_mangers/manager_creates_team_spec.rb
@@ -5,10 +5,11 @@ describe 'Teams' do
     @user = create(:user)
     @company = create(:company)
     @production = create(:production, company: @company)
-    create(:company_manager, company: @company, user: @user)
   end
 
   it 'managers can create teams' do
+    create(:company_manager, company: @company, user: @user)
+    stub_login_user(@user)
     visit company_production_path(@company.name, @production.title)
     click_on 'Create a new team'
     fill_in 'team_name', :with => 'Costume Department'
@@ -17,5 +18,14 @@ describe 'Teams' do
     expect(current_path).to eq(company_production_path(@company.name, @production.title))
     expect(page).to have_content('Your team was created!')
     expect(page).to have_content('Costume Department')
+  end
+
+  it 'non-managers cannot create teams' do
+    team = create(:team, production: @production)
+    create(:membership, user: @user, team: team)
+    stub_login_user(@user)
+    visit company_production_path(@company.name, @production.title)
+
+    expect(page).to_not have_content('Create a new team')
   end
 end
